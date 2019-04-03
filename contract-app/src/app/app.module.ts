afbd6b4395beb8be1579cec978718eb2f16d8d26
endy <endy.brosens@brandloyalty-int.com>
2019-04-03 10:23:25 +0200

    Display table of contracts.

--- contract-app/src/app/app.module.ts
+++ contract-app/src/app/app.module.ts
@@ -4,13 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatTableModule} from '@angular/material';
+import { ContractsComponent } from './contracts/contracts.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContractsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
